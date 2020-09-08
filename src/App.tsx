7aa631ba0902297a285fda27b1cbc13e6079e368
ewami <efili1989@gmail.com>
2020-09-08 21:29:29 +0200

    make grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Grid } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a>Thai Script Cards </a>
-      </header>
-    </div>
+    <Grid container direction="column">
+      <Grid item>header will be here</Grid>
+      <Grid item container>
+        <Grid item xs={2} sm={2} />
+        <Grid item xs={12} sm={8}>
+          content will be here content will be here content will be here content
+          will be here content will be here content will be here content will be
+          here content will be here content will be here content will be here
+        </Grid>
+        <Grid item xs={2} sm={2} />
+      </Grid>
+    </Grid>
   );
 }
 
