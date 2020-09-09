db46f08cbf7bb63ae0b83722de683653ff18b273
ewami <efili1989@gmail.com>
2020-09-09 17:10:04 +0200

    update constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -2,40 +2,54 @@ export default [
   {
     image:
       "https://cdn.pixabay.com/photo/2015/04/20/13/28/lizard-731336_1280.jpg",
-    title: "Lizard",
+    title: "Gecko",
     header: "จิ้งจก",
+    engHeader: "Gecko",
     content:
-      "ชื่อสัตว์เลื้อยคลานขนาดเล็กในวงศ์ Gekkonidae ซึ่งเป็นวงศ์เดียวกับตุ๊กแกในประเทศไทยมีอยู่หลายชนิด.",
+      "ชื่อสัตว์เลื้อยคลานขนาดเล็กในวงศ์ Gekkonidae ซึ่งเป็นวงศ์เดียวกับตุ๊กแกในประเทศไทยมีอยู่หลายชนิด พบมากในแถบเอเชียตะวันออกเฉียงใต้ และแพร่พันธุ์ไปในหลายประเทศทั่วโลก ทั้งแอฟริกาตะวันออก, นิวกินี, เม็กซิโก, มาดากัสการ์, ออสเตรเลีย และหลายพื้นที่ทั่วโลก ",
+    engContent:
+      "There are several small reptiles in the Gekkonidae family that belong to the geckos in Thailand. It is found mostly in Southeast Asia. And propagated in many countries around the world East Africa, New Guinea, Mexico, Madagascar, Australia and many areas around the world.",
   },
   {
     image: "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
     title: "Cat",
     header: "แมว",
+    engHeader: "Cat",
     content:
       "ชื่อสัตว์เลี้ยงลูกด้วยน้ำนม ๔ เท้าชนิด Felis domestica ในวงศ์ Felidae ซึ่งเป็นวงศ์เดียวกับเสือ ส่วนใหญ่เลี้ยงเป็นสัตว์เลี้ยง เพื่อความเพลิดเพลิน และใช้จับหนู",
+    engContent:
+      "The four-footed mammal Felis domestica in the Felidae family, which is the same family as the tiger. Most of them are domesticated as pets for pleasure and used to catch mice.",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2018/08/12/16/59/ara-3601194_1280.jpg",
     title: "Parrot",
     header: "นกแก้ว",
+    engHeader: "Parrot",
     content:
-      "เป็นนกที่มีความแตกต่างกันมากทางสรีระ คือมีตั้งแต่ขนาดเล็กจนถึงใหญ่ (19-100 เซนติเมตร) มีหัวกลมโต ลำตัวมีขนอุยปกคลุมหนาแน่น ขนมีแกนขนรอง ต่อมน้ำมันมีลักษณะเป็นพุ่มขน ผิวหนังค่อนข้างหนา.",
+      "เป็นนกที่มีความแตกต่างกันมากทางสรีระ คือมีตั้งแต่ขนาดเล็กจนถึงใหญ่ (19-100 เซนติเมตร) มีหัวกลมโต ลำตัวมีขนอุยปกคลุมหนาแน่น ขนมีแกนขนรอง ต่อมน้ำมันมีลักษณะเป็นพุ่มขน ผิวหนังค่อนข้างหนา",
+    engContent:
+      "They are birds that are very different in their physiology. They range from small to large (19-100 cm), with a large round head, densely covered, hairy body, axial hairs, secondary oil glands, bushy hair. They have thick skin.",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2016/10/01/20/54/mouse-1708347_1280.jpg",
     title: "Mouse",
-    header: "เมาส์",
+    header: "หนู",
     content:
-      "อุปกรณ์ที่มีลักษณะเป็นกล่องพลาสติกขนาดเล็กรูปมน ๆ คล้ายหนู มีสายต่อพ่วงกับคอมพิวเตอร์เพื่อชี้ตำแหน่งบนจอภาพ และควบคุมการทำงานของเครื่องคอมพิวเตอร์",
+      "ชื่อสัตว์เลี้ยงลูกด้วยน้ำนมหลายสกุลในวงศ์ Muridae มีฟันแทะ อาศัยอยู่ทั่วไปตามบ้านเรือน และในธรรมชาติ มีหลายชนิด",
+    engContent:
+      "Many types of mammals of the Muridae family are called domestic rodents. There are many kinds of them.",
   },
   {
     image:
       "https://cdn.pixabay.com/photo/2016/02/26/16/32/dog-1224267_1280.jpg",
     title: "Dog",
     header: "หมา",
+    engHeader: "Dog",
     content:
-      "ชื่อสัตว์เลี้ยงลูกด้วยน้ำนมหลายชนิดหลายสกุลในวงศ์ Canidae ลำตัวมีขนปกคลุมมีเขี้ยว ๒ คู่ มีทั้งที่เป็นสัตว์ป่า และสัตว์เลี้ยง",
+      "แม้ว่าเดิมทีจะคิดกันว่าหมามีต้นกำเนิดจากสายพันธุ์สัตว์มีเขี้ยวที่หลากหลาย มีการศึกษาทางพันธุศาสตร์เพิ่มเติมในคริสต์ทศวรรษ 2010 ชี้ว่าหมาเริ่มแตกต่างจากสัตว์มีเขี้ยวคล้ายหมาป่าในยูเรเชียเมื่อ 40,000 ปีที่แล้ว",
+    engContent:
+      "Although it was originally thought that dogs originated in animal breeds, they had a wide variety of fangs. Additional genetic studies in the 2010s indicated that dogs began to differ from wolf-like fangs in Eurasia 40,000 years ago.",
   },
 ];
